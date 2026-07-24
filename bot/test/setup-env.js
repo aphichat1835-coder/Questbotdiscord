cf6d2fa4040dd0f05c381e30dbce7be2e6fe1cc0
aphichat1835-coder <aphichat1835@gmail.com>
2026-07-25 02:37:06 +0700

    Add shared environment defaults for tests

---
+++ bot/test/setup-env.js
@@ -0,0 +1,6 @@
+process.env.DISCORD_BOT_TOKEN ||= 'test-bot-token';
+process.env.DISCORD_CLIENT_ID ||= '12345678901234567';
+process.env.DISCORD_GUILD_ID ||= '22345678901234567';
+process.env.OWNER_ID ||= '32345678901234567';
+process.env.RUNNER_TOKEN_SECRET ||= 'test-runner-token-secret-32-characters';
+process.env.LOG_WEBHOOK_URL ||= 'https://discord.com/api/webhooks/42345678901234567/test_webhook_token_abcdefghijklmnopqrstuvwxyz';
