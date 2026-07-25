9acd6b2984bf0922a59b2f6281441f0873b712b1
aphichat1835-coder <aphichat1835@gmail.com>
2026-07-26 02:51:35 +0700

    Fix isolated test webhook environment

--- bot/test/setup-env.js
+++ bot/test/setup-env.js
@@ -6,4 +6,3 @@ process.env.DISCORD_GUILD_ID = '22345678901234567';
 process.env.OWNER_ID = '32345678901234567';
 process.env.RUNNER_TOKEN_SECRET = 'test-runner-token-secret-32-characters';
 process.env.LOG_WEBHOOK_URL = createFakeDiscordWebhookUrl('shared');
-process.env.QUESTBOT_TEST_MODE = 'true';
