c6d223dfdc9bdd72914f109d1f75a03188c329b3
aphichat1835-coder <aphichat1835@gmail.com>
2026-07-27 14:50:31 +0700

    feat(quest): centralize Quest API endpoints

---
+++ bot/src/quest/api/quest-endpoints.js
@@ -0,0 +1,18 @@
+export const QUEST_LIST_PATHS = Object.freeze([
+  '/quests/@me',
+  '/users/@me/quests',
+]);
+
+export const QUEST_ENDPOINT = Object.freeze({
+  me: () => '/users/@me',
+  enroll: (questId) => `/quests/${questId}/enroll`,
+  videoProgress: (questId) => `/quests/${questId}/video-progress`,
+  heartbeat: (questId) => `/quests/${questId}/heartbeat`,
+  claimReward: (questId) => `/quests/${questId}/claim-reward`,
+  claimLegacy: (questId) => `/quests/${questId}/claim`,
+});
+
+export const FATAL_FORBIDDEN_PATHS = new Set([
+  QUEST_ENDPOINT.me(),
+  ...QUEST_LIST_PATHS,
+]);
